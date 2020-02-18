4bf26d6d589ae3078c9ae568e61f2160b5bad10d
ivonne2909 <61174987+ivonne2909@users.noreply.github.com>
2020-02-18 00:27:43 -0600

    Add files via upload

---
+++ ProyectoReloj/src/proyectoreloj/ProyectoReloj.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proyectoreloj;
+
+/**
+ *
+ * @author Ivon2
+ */
+public class ProyectoReloj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Reloj prueba = new Reloj(12,13,01);
+         prueba.mostrarhora(); // Llamamos al metodo mostrarHora()
+                
+    }
+    
+}
+
+        
+        // TODO code application logic 
---
+++ ProyectoReloj/src/proyectoreloj/Reloj.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proyectoreloj;
+
+/**
+ *
+ * @author Ivon2
+ */
+public class Reloj {
+ private int hora;
+    private int minuto;
+    private int segundo;
+    
+    public Reloj(int hora ,int minuto , int segundo ){
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo = segundo;
+    }
+   
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
+
+    public int getSegundo() {
+        return segundo;
+    }
+
+    public void setSegundo(int segundo) {
+        this.segundo = segundo;
+    }
+     public void mostrarhora(){
+        
+    }
+       incrementarSegundo();
+       
+       try {
+    Thread.sleep(60);
+    
+    catch(InterruptedException e) {
+            }
+}
+       
+    
+}
